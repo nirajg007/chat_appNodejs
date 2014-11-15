8c9a8b9ab0574bc60c1695c2802a0f2e3f7aa51b
Niraj Kumar <nirajg007@gmail.com>
2014-11-15 16:38:58 +0530

    edit

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ app.get('/', function(req, res){
    res.sendFile(__dirname + '/index.html');
 });
 
-
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 http.listen(8080, function(){
   console.log('listening on *:8080');
 });

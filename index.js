3fa60295ee7b30744c9ed0e267616300fba299b6
Niraj Kumar <nirajg007@gmail.com>
2014-11-15 16:37:56 +0530

    edit

---
+++ index.js
@@ -0,0 +1,12 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+   res.sendFile(__dirname + '/index.html');
+});
+
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
